acd25e87527371d7f33fe18b02f8436272ad2c9f
XxfloidxX <jpzinhoo-melo@hotmail.com>
2013-12-18 16:20:21 -0200

    Create index.php

---
+++ texto/index.php
@@ -0,0 +1,16 @@
+<?php
+$x = 9;
+$y = 12;
+
+$dia = 11;
+$mes = 12;
+$ano = 2013;
+$texto = "o data de hoje é ";
+
+echo $texto . $dia . "/" . $mes . "/" . $ano;
+echo "<br/>";
+echo "o dia de hoje é $dia/$mes/$ano";
+echo "<br/>";
+echo "$x + $y = " . ($x + $y);
+
+?>
