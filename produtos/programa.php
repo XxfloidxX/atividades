eb3fce2d52aa03c7a24b683f8bdf417203b7911a
XxfloidxX <jpzinhoo-melo@hotmail.com>
2013-12-18 16:18:30 -0200

    Create programa.php

---
+++ produtos/programa.php
@@ -0,0 +1,20 @@
+<?php
+$codigo = 0;
+
+$codigo = $_POST["codigo"];
+
+
+if ($codigo == 1){
+      echo "caneta 1.20";
+}
+else if ($codigo == 2){
+      echo "caderno 4.50";
+}
+else if ($codigo == 3){
+      echo "lapis 0.80";
+{
+
+else {
+	echo "produto não cadastrado";
+}
+?>
